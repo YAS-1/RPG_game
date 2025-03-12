0209196a436c47080e58e09a3c380b54e0b3ecd2
nabasaisaac <nabasaisaac16@gmail.com>
2025-03-12 21:23:22 +0300

    B1

--- rpg_game.js
+++ rpg_game.js
@@ -8,7 +8,7 @@ let fighting;
 let monsterHealth;
 let inventory = [`stick`]; //comment
 
-const button1 = document.getElementById("button1");
+const button1 = document.getElementById("button1"); //button
 const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
 const text = document.getElementById("text");
